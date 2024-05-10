42f2649002090693ad7c674e89137cc383a95794
swajid <imsanaw@gmail.com>
2024-05-10 15:04:06 -0400

    -

---
+++ workflow/rules/checkm.smk.py
@@ -0,0 +1,21 @@
+rule all:
+    input:
+        "checkm/output/qa_summary.tsv"
+
+rule run_checkm_lineage_wf:
+    input:
+        GENOMES_DIR = "../../test/"
+    output:
+        "checkm/output/qa_summary.tsv"
+    params:
+        output_dir = "checkm/output",
+        tmp_dir = "checkm/tmp",
+        threads = 90
+    shell:
+        """
+        checkm lineage_wf --tmpdir {params.tmp_dir} -x fa -t {params.threads} \
+            {input.GENOMES_DIR} {params.output_dir}
+
+        checkm qa {params.output_dir}/lineage.ms {params.output_dir} \
+            --file {output} -t {params.threads}
+        """
